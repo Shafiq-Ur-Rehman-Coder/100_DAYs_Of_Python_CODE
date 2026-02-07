f89baf3c99a9f0f41edd6f85ac0a5853d8601bdf
Shafiq Ur Rehman <codewithshafiq49@gmail.com>
2026-02-07 16:27:44 +0100

    Add files via upload

---
+++ Python code 100 [Days]/Day_4.py
@@ -0,0 +1,20 @@
+# a = "hello"  #|| str = string inteager= int||
+# print(type(a))
+# # without varibles
+# print(type("hello"))
+
+# a = "13" 
+# b = int(a)
+# print(type(b))
+# print((str(13)))
+
+
+
+# ||| input, output
+
+a = input("enter your name> ")
+# print("hello",a)
+if a == "shafiq":
+    print("good")
+else:
+    print("hi")
